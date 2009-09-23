6642ec16fd5b1db8a4ae5d288a6cdf1d1d578dac
Daniel Morrison <daniel@collectiveidea.com>
2009-09-23 10:42:57 -0400

    Allow installation as a gem

---
+++ lib/deploy_and_deliver/recipes.rb
@@ -0,0 +1,41 @@
+Capistrano::Configuration.instance.load do
+  
+  namespace :pivotal_tracker do
+    desc "deliver your project's 'finished' stories"
+    task :deliver_stories do
+      require 'rubygems'
+      require 'activeresource'
+
+      class Story < ActiveResource::Base
+        self.site = "http://www.pivotaltracker.com/services/v2/projects/:project_id"
+      end
+
+      Story.headers['X-TrackerToken'] = pivotal_tracker_token
+      puts "* delivering tracker stories ..."
+      response = Story.put(:deliver_all_finished, :project_id => pivotal_tracker_project_id)
+
+      begin
+        require 'sax-machine'
+        class Story
+          include SAXMachine
+          element :name
+          element :story_type
+          element :estimate
+          element :description
+        end
+        class Stories
+          include SAXMachine
+          elements :story, :as => :stories, :class => Story
+        end
+        doc = Stories.parse(response.body)
+        puts "* delivered #{doc.stories.length} stories"
+        doc.stories.each do |story|
+          puts "  - #{story.story_type}: #{story.name} (#{story.estimate} points)"
+        end
+      rescue LoadError => e
+        puts "* stories delivered."
+      end
+    end
+  end
+  
+end

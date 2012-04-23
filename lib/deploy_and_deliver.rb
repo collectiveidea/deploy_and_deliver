d7e4375d0235f541f583f0dc289ded2a4bdbd841
Jon Kinney <jonkinney@gmail.com>
2012-04-23 12:35:26 -0500

    Fixes the summing of story points during deliver
    - Not sure if this was a 1.9.3 issue or what, but the inject didn't work
    on my system as coded.

--- lib/deploy_and_deliver.rb
+++ lib/deploy_and_deliver.rb
@@ -9,16 +9,16 @@ module DeployAndDeliver
       PivotalTracker::Client.token = context[:pivotal_tracker_token]
       @project = PivotalTracker::Project.find(context[:pivotal_tracker_project_id])
     end
-  
+
     def deliver_and_report
       stories = project.stories.all(:current_state => 'finished')
       stories.each{|story| story.update :current_state => 'delivered'}
-      
-      puts "* delivered #{stories.size} stories (#{stories.inject{|sum=0, story| sum + story.estimate}} points)"
+
+      puts "* delivered #{stories.size} stories (#{stories.map(&:estimate).inject(:+)} points)"
       stories.each do |story|
         puts "  - #{story.story_type.capitalize}: #{story.name} (#{story.estimate} #{story.estimate == 1 ? "point" : "points"})"
       end
     end
   end
-    
+
 end
